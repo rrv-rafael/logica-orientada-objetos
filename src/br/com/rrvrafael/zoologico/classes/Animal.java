b25e15aab7119a6ce6738bcc45ba34d540a3e613
Rafael Rodrigues Vitor <rrv.rafael@outlook.com>
2022-04-19 20:20:02 -0300

    continuidade do curso

---
+++ src/br/com/rrvrafael/zoologico/classes/Animal.java
@@ -0,0 +1,35 @@
+package br.com.rrvrafael.zoologico.classes;
+
+public class Animal {
+    private String nome;
+    private String especie;
+    private int idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public boolean animalAdulto() {
+        return idade >= 1;
+    }
+}
